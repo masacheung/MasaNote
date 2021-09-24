24ede1fc9e965a7b96f572c5afc1cee460b51675
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-09-24 14:35:58 -0400

    Root Page

---
+++ frontend/masa_note.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById('root')
+  ReactDOM.render(<h1>My super awesome app is on it's way...</h1>, root)
+});
