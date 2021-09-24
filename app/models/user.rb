2906870d3db927e0e7379968a8c3fb112d5cfd0f
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-09-24 18:32:39 -0400

    Session Controller User Controller

---
+++ app/models/user.rb
@@ -0,0 +1,40 @@
+class User < ApplicationRecord
+
+    validates :email, :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: {minimum: 6}, allow_nil: true
+
+    attr_reader :password
+
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            return user
+        else
+            return nil
+        end
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        sekf.session_token
+    end
+
+    private
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
+
+end
