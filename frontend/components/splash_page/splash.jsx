a501eb667a28ee3eeac24c2bf7d26e2ec91a30e5
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-09-28 12:39:36 -0400

    splash page styleing

--- frontend/components/splash_page/splash.jsx
+++ frontend/components/splash_page/splash.jsx
@@ -2,8 +2,43 @@ import React from 'react';
 
 const Splash = () => {
     return (
-        <div>
-            Splash Page
+        <div className="splash">
+            <div>
+                Spash Page
+            </div>
+            <div className="splash-footer">
+                <div className="splash-info">
+                    <h3>
+                        About
+                    </h3>
+                    <ul>
+                        <li>
+                            Man Tat Masa Cheung
+                        </li>
+                    </ul>
+                </div>
+                <div className="splash-info">
+                    <h3>
+                        Contact
+                    </h3>
+                    <ul>
+                        <li>
+                            <a href="https://github.com/masacheung">Github</a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/man-tat-masa-cheung-725b39b8/">LinkedIn</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="splash-info">
+                    <h3>More Projects</h3>
+                        <ul>
+                            <li>
+                                <a href="https://masacheung.github.io/dropping_down/">Dropping Down</a>
+                            </li>
+                        </ul>
+                </div>
+            </div>
         </div>
     )
 }
