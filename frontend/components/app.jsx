12093a031d47acd244020679c0383b2ac1c1d803
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-09-28 12:17:42 -0400

    Splash page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Switch} from "react-router-dom";
 import LoginFormContainer from "./user_auth/login_form_container";
 import SignupFormContainer from "./user_auth/signup_form_container";
+import Splash from "./splash_page/splash";
 
 import {AuthRoute, ProtectedRoute} from '../util/route_util';
 
@@ -10,6 +11,7 @@ const App = () => (
         <Switch>
             <AuthRoute path="/login" component={LoginFormContainer} />
             <AuthRoute path="/signup" component={SignupFormContainer}/>
+            <AuthRoute path="/" component={Splash}/>
         </Switch>
     </div>
 )
---
+++ frontend/components/splash_page/splash.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Splash = () => {
+    return (
+        <div>
+            Splash Page
+        </div>
+    )
+}
+
+export default Splash;
