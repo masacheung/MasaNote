1b26799e8140ccff4969b6fe04a799c07a51acfe
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-09-28 14:58:24 -0400

    Splash Page

--- frontend/components/splash_page/splash.jsx
+++ frontend/components/splash_page/splash.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Splash = () => {
     return (
         <div className="splash">
-            <div>
+            <div className="splash-slogan">
                 Spash Page
             </div>
             <hr/>
