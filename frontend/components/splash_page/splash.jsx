da02e16b300642f33516f76ad9c53536a120adcf
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-10-01 16:57:23 -0400

    Splash page

--- frontend/components/splash_page/splash.jsx
+++ frontend/components/splash_page/splash.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Splash = () => {
     return (
         <div className="splash">
             <div className="splash-slogan">
-                Splash Page
+                <div className="slogan">
+                    Power of MasaNote, Make your life easier
+                </div>
+                <h1 className="sec-slogan">
+                    Remember Masa is the creator.
+                </h1>
+                    <Link to="/signup" className="slogan-sign-up-for-free">
+                        <button>Sign up for free</button>
+                    </Link>
+                    <br/>
+                    <div className="slogan-log-in-link">
+                    <Link to="/login">
+                        <button >Already have an account? Log in</button>
+                    </Link>
+                    </div>
             </div>
             <hr/>
             <div className="splash-footer">
