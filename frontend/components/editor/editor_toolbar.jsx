2ac5e16351e2317ba5fcefd6a1c1fd0eb43a032d
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-10-05 13:03:41 -0400

    editor toolbar

--- frontend/components/editor/editor_toolbar.jsx
+++ frontend/components/editor/editor_toolbar.jsx
@@ -5,3 +5,33 @@ const Size = Quill.import("formats/size");
 Size.whitelist = ["small", "medium", "large"];
 Quill.register(Size, true);
 
+const Font = Quill.import("formats/font");
+Font.whitelist = ["Sans-Serif", "Serif", "Monospace", "Script"];
+Quill.register(Font, true);
+
+export const format = [
+    "header",
+    "font",
+    "size",
+    "strike",
+    "bold",
+    "italic",
+    "underline",
+    "script",
+    "blockquote",
+    "background",
+    "list",
+    "bullet",
+    "ident",
+    "link",
+    "color",
+    "code-block"
+];
+
+const EditorToolbar = ({ showToolbar }) => (
+    <div>
+        
+    </div>
+)
+
+export default EditorToolbar;
