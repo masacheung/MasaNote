aea24dc8475e7ad7afb4e7dc4b2b56e6edd26de1
Masa Cheung <63529499+masacheung@users.noreply.github.com>
2021-10-07 22:08:17 -0400

    Display date

--- frontend/components/notes/note_item.jsx
+++ frontend/components/notes/note_item.jsx
@@ -30,13 +30,27 @@ const NoteItem = props => {
             displayDate = `${temp} minutes ago`;
         }else {
             let temp = dataNow.getHours() - updateDate.getHours();
-            displayDate = temp;
+            displayDate = `${temp} hours ago`;
         }
-    }else if(dateNow.getDate() - updateDate.getDate() === 1){
+    }else if((dateNow.getDate() - updateDate.getDate() === 1) && (dateNow.getMonth() === updateDate.getMonth())){
         displayDate = "Yesterday"
     }else {
         displayDate = formatDateTime(date);
     }
+    // if((dateNow.getDate() === updateDate.getDate()) && (dateNow.getMonth() === updateDate.getMonth())){
+    //     if (dateNow.getMinutes() === updateDate.getMinutes()){
+    //         displayDate = "a few second ago";
+    //     }else if (dateNow.getMinutes() - updateDate.getMinutes() < 10){
+    //         displayDate = "a few minutes ago";
+    //     }else if (dateNow.getHours() === updateDate.getHours()){
+    //         let temp = dateNow.getMinutes() - updateDate.getMinutes();
+    //         displayDate = `${temp} minutes ago`;
+    //     }else {
+    //         displayDate = formatDateTime(date);
+    //     }
+    // }else {
+    //     displayDate = formatDateTime(date);
+    // }
 
     const body = props.note.body.slice(0,100).replace(/<[^>]*>?/gm, '');
 
